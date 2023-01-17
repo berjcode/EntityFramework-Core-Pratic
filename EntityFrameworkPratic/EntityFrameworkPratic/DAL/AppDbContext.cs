e9732e647d07c06284e7430fe16638e71e09c313
Abdullah Balıkçı <abdullah.balikci20@ogr.atauni.edu.tr>
2023-01-18 01:34:59 +0300

    Update

---
+++ EntityFrameworkPratic/EntityFrameworkPratic/DAL/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkPratic.DAL
+{
+    public class AppDbContext:DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=berjcode;Initial Catalog=EntityFrameworkPratic;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
--- EntityFrameworkPratic/EntityFrameworkPratic/Program.cs
+++ EntityFrameworkPratic/EntityFrameworkPratic/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using EntityFrameworkPratic.DAL;
+
 Console.WriteLine("Hello, World!");
+
+using (var _context = new AppDbContext())
+{
+    var products = _context.Products.ToList();
+
+    foreach(var item in products)
+    {
+        Console.Write($"Ürün Adı: {item.Name}, Ürün Tutaru: {item.Price} \n"  );
+    }
+
+}
